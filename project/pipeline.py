125e72c002fe5330a4a4f661056a5be2e3cf8344
Nahrain Gtari <nahraingtari1@gmail.com>
2023-12-04 23:10:33 +0100

    changes necessary in pipeline.py for testing

--- project/pipeline.py
+++ project/pipeline.py
@@ -2,10 +2,11 @@ from sqlalchemy import create_engine
 import pandas as pd
 import sqlalchemy as sa
 import os
+import numpy as np 
 
 def clean_dataset(url, n):
 
-    database_path = os.path.join("C://Users//nahra//OneDrive//Desktop//Master//made-template//data",f"table{n}.sqlite")
+    database_path = os.path.join("C:\\Users\\nahra\\OneDrive\\Desktop\\Master\\made-template\\data",f"table{n}.sqlite")
     engine = create_engine(f"sqlite:///{database_path}")
     meta = sa.MetaData()
     meta.drop_all(bind=engine)
@@ -19,24 +20,29 @@ def clean_dataset(url, n):
         replacements = {'c': '', '(': '', ')': ''}
         df = df.replace(to_replace=replacements)
         df = df.replace(to_replace=r'\((\d+)\)', value=r'\1', regex=True)
-        df.to_sql(name='table1', con=engine, if_exists="replace", index=False)   
- 
+        df.to_sql(name='table1', con=engine, if_exists="replace", index=False)  
+       
     elif n==2:
         df = pd.read_csv(url, delimiter=';',skiprows=range(53, 58091), nrows=53,encoding='latin1')
+        df = df.drop(df.columns[0], axis=1)
         df.to_sql(name='table2', con=engine, if_exists="replace", index=False)
-
+      
     else: 
         df = pd.read_csv(url, delimiter=';',skiprows=range(0, 3), nrows=35,encoding='latin1')
-        # Rename the "Unnamed: 0" column to None
+        # Rename the "Unnamed: 0" column to Years
         df.rename(columns={"Unnamed: 0": "Years"}, inplace=True)
-        df.fillna("", inplace=True)
+        df.fillna(0, inplace=True)
+        df['Years'] = df['Years'].astype(int)
         df.to_sql(name='table3', con=engine, if_exists="replace", index=False)
+        
+    return df
             
 url1 = "https://www.datenportal.bmbf.de/portal/Tabelle-0.64.csv"
-clean_dataset(url1, 1)
+df1 =clean_dataset(url1, 1)
+
 
 url2 = "https://www.bka.de/SharedDocs/Downloads/DE/Publikationen/PolizeilicheKriminalstatistik/2022/Land/Tatverdaechtige/LA-TV-03-T40-Laender-TV-deutsch_csv.csv?__blob=publicationFile&v=4"
-clean_dataset(url2, 2)
+df2 = clean_dataset(url2, 2)
 
 url3 = "https://www-genesis.destatis.de/genesis/downloads/00/tables/13211-0009_00.csv"
-clean_dataset(url3, 3)
+df3 = clean_dataset(url3, 3)
