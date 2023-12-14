8b6685f78b8fb59253fef651bce5b0a4b22c044f
Nahrain Gtari <nahraingtari1@gmail.com>
2023-12-14 20:29:56 +0100

    Update tests.py

--- project/tests.py
+++ project/tests.py
@@ -22,7 +22,8 @@ def pipeline_for_dataset3():
     url = "https://www-genesis.destatis.de/genesis/downloads/00/tables/13211-0009_00.csv"
     return clean_dataset(url=url, n=3)
 
-store_path = r"C:\\Users\\nahra\\OneDrive\\Desktop\\Master\\made-template\\data"
+#store_path = r"C:\\Users\\nahra\\OneDrive\\Desktop\\Master\\made-template\\data"
+store_path = sa.create_engine('sqlite:///:memory:')
 
 def test_check_df_instance (pipeline_for_dataset1, pipeline_for_dataset2, pipeline_for_dataset3):
     df1 = pipeline_for_dataset1
@@ -44,11 +45,13 @@ def test_check_nulls (pipeline_for_dataset1, pipeline_for_dataset2, pipeline_for
 
 def test_table_existence():
     database_path1 = os.path.join(store_path, "table1.sqlite")
-    assert os.path.exists(database_path1)
+    #assert os.path.exists(database_path1)
+    assert store_path.has_table('table1')
     database_path2 = os.path.join(store_path, "table2.sqlite")
     assert os.path.exists(database_path2)
     database_path3 = os.path.join(store_path, "table3.sqlite")
     assert os.path.exists(database_path3)
+    
 
 
 def test_data_types_sample_col(pipeline_for_dataset1, pipeline_for_dataset2, pipeline_for_dataset3):
@@ -58,7 +61,7 @@ def test_data_types_sample_col(pipeline_for_dataset1, pipeline_for_dataset2, pip
     assert df2['4'].apply(lambda x: isinstance(x, str)).all()
     assert df2['6'].apply(lambda x: isinstance(x, str)).all()
     df3 = pipeline_for_dataset3
-    assert df3['Years'].dtype == np.int32
+    assert df3['Years'].dtype == np.int64
 
 
 def test_df_shape(pipeline_for_dataset1, pipeline_for_dataset2, pipeline_for_dataset3):
